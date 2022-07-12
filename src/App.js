d306de960ac3819b486f13a4ddd0d76e7a422b94
pidokige02 <pidokige0204@gmail.com>
2022-07-12 22:02:51 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -11,20 +11,50 @@ function App() {
         setLoading(false);
       });
   }, []);
+
+  //input
+  const [money, setMoney] = useState(0);
+  const onChange = (event) => {
+      setMoney(event.target.value);
+  };
+  //coin
+  const [coinValue, setCoinValue] = useState(0);
+  const changeMoney = (event) => {
+    setCoinValue(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={changeMoney}>
           {coins.map((coin) => (
-            <option>
+            <option value={coin.quotes.USD.price}>
               {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+    <hr />
+    <form action=''>
+        <label htmlFor='input'>
+          {" "}
+          <strong> Write the money you have</strong>
+        </label>
+        <br />
+        <input
+          id='input'
+          type='number'
+          placeholder='Write the money'
+          value={money}
+          onChange={onChange}
+        />
+
+        <h3>You can buy {(money / coinValue).toFixed(1)} coin</h3>
+      </form>
     </div>
   );
 }
